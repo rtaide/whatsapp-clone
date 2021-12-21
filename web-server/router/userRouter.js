1a7bf5975e01c04a6dfa68a569d065108d44c391
Rachit Taiden <rachit.taide.551832@gmail.com>
2021-12-21 15:37:23 +0530

    updated

--- web-server/router/userRouter.js
+++ web-server/router/userRouter.js
@@ -1,11 +1,7 @@
 //export {};
 const express =require ("express");
-<<<<<<< Updated upstream
-const { loginUser, getLoggedInUserList, searchUserByName } =require ("../controller/authController");
-=======
-const { loginUser, getLoggedInUserList, profile, deleteAccount,updateProfile } =require ("../controller/authController");
+const { loginUser, getLoggedInUserList, profile, deleteAccount,updateProfile ,searchUserByName} =require ("../controller/authController");
 //const { updateProfile } = require("../controller/profile");
->>>>>>> Stashed changes
 const {authUser} =require ("../middleware/auth");
 
 const router = express.Router();
